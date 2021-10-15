2b80563eba5834da3e1b6193fdca260f1d18b803
Marcosss2 <91058545+Marcosss2@users.noreply.github.com>
2021-10-15 15:31:53 +0200

    n

---
+++ ManejoExcepcion/src/manejoexcepcion/Test.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package manejoexcepcion;
+
+
+public class Test {
+
+   
+    public static void main(String[] args) {
+        
+        
+        
+        int res = 0;
+        try {
+           res = 10/0;
+            
+        } catch (Exception e) {
+        
+         e.printStackTrace( System.out);
+        
+        }
+        
+        
+    
+        
+        
+    }
+    
+}
